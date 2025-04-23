4d4bf0fd0c61126e2e741368688360a2b67d989a
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-04-22 21:20:21 -0300

    resolvi ejercicio 12

---
+++ js/ejercicio12.js
@@ -0,0 +1,7 @@
+/*12- Realiza un script que genere un número aleatorio entre 1 y 99
+usando math
+var num = Math.floor(Math.random() * ((máximo+1) - mínimo) + mínimo);
+*/
+
+const numero=Math.floor(Math.random()*(99+1))
+document.writeln(numero)
